c0ac3cd806dd21d32f56bc79f3c2f78f134aaa4e
Keane <KeaneCarotenuto@gmail.com>
2024-01-19 23:19:44 +1300

    Fixed Crouch + cam step bobbing

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class Player_Movement : MonoBehaviour
 {
@@ -27,9 +27,19 @@ public class Player_Movement : MonoBehaviour
     public float m_crouchHeightMulti = 0.5f;
     public float m_crouchDownTime = 0.25f;
     public float m_crouchUpTime = 0.5f;
+    public float m_currentHeight = 1f;
+    public float m_normalHeight = 1f;
 
     [Header("Look")]
     public float m_mouseSensitivity = 1f;
+    float m_camY = 0;
+
+    [Header("Steps")]
+    [SerializeField] float stepHeight = 0.05f;
+    [SerializeField] float stepLength = 1.0f;
+    [SerializeField] float distanceTraveled = 0;
+    public UnityEvent Step;
+    bool oneStep = true;
 
     [Header("Gravity")]
     public float m_gravity = -9.81f;
@@ -51,6 +61,11 @@ public class Player_Movement : MonoBehaviour
         Cursor.visible = false;
     }
 
+    private void Start()
+    {
+        m_camY = m_cam.transform.localPosition.y;
+    }
+
     private void Update()
     {
         // mouse look
@@ -67,7 +82,6 @@ public class Player_Movement : MonoBehaviour
         Vector3 moveDir = (transform.right * x + transform.forward * z).normalized;
 
         // Sprint & Crouch
-        float height = 1f;
         if (InputManager.PlayerMove.Sprint.IsPressed())
         {
             moveDir *= m_sprintSpeedMultiplier;
@@ -78,30 +92,38 @@ public class Player_Movement : MonoBehaviour
 
             // crouch down
             float currentHeight = transform.localScale.y;
-            if (currentHeight > height * m_crouchHeightMulti)
+            if (currentHeight > m_normalHeight * m_crouchHeightMulti)
             {
-                // lerp between full height and crouch height
-                transform.localScale = Vector3.Lerp(new Vector3(1, height * m_crouchHeightMulti, 1), new Vector3(1, height, 1), m_crouchDownTime * Time.deltaTime);
+                // subtract from current height (after m_crouchDownTime seconds, we should be at m_normalHeight * m_crouchHeightMulti)
+                float toSubtract = (m_normalHeight - (m_normalHeight * m_crouchHeightMulti)) / m_crouchDownTime * Time.deltaTime;
+                m_currentHeight -= toSubtract;
 
-                // move player down with crouch
-                m_controller.Move(Vector3.down * m_moveSpeed * Time.deltaTime);
+                // move player down
+                m_controller.Move(Vector3.down * toSubtract * 2f);
             }
         }
         else
         {
             // crouch up
             float currentHeight = transform.localScale.y;
-            if (currentHeight < height)
+            if (currentHeight < m_normalHeight)
             {
-                // lerp between full height and crouch height
-                transform.localScale = Vector3.Lerp(new Vector3(1, height * m_crouchHeightMulti, 1), new Vector3(1, height, 1), m_crouchUpTime * Time.deltaTime);
+                // add to current height (after m_crouchUpTime seconds, we should be at m_normalHeight)
+                float toAdd = (m_normalHeight - currentHeight) / m_crouchUpTime * Time.deltaTime;
+                m_currentHeight += toAdd;
 
-                // move player up with crouch
-                m_controller.Move(Vector3.up * m_moveSpeed * Time.deltaTime);
+                // move player up
+                m_controller.Move(Vector3.up * toAdd * 2f);
             }
         }
 
-        m_controller.Move(moveDir * m_moveSpeed * Time.deltaTime);
+        // Apply Height
+        Vector3 currentScale = transform.localScale;
+        currentScale.y = m_currentHeight;
+        transform.localScale = currentScale;
+
+        Vector3 moveVector = moveDir * m_moveSpeed * Time.deltaTime;
+        m_controller.Move(moveVector);
 
         // Jump
         if (InputManager.PlayerMove.Jump.WasPerformedThisFrame() && m_isGrounded)
@@ -123,6 +145,31 @@ public class Player_Movement : MonoBehaviour
         else if (m_velocity.y > 0 && !InputManager.PlayerMove.Jump.IsPressed())
         {
             m_velocity += Vector3.up * m_gravity * (m_lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+        // Steps
+        if (m_isGrounded)
+        {
+            distanceTraveled += new Vector3(moveVector.x, 0, moveVector.z).magnitude;
+        }
+        m_cam.transform.localPosition =
+            (transform.up * m_camY * (1.0f - Mathf.Sin(distanceTraveled * (1.0f / stepLength))) * stepHeight)
+            + (transform.up * m_camY)
+            + (transform.right * (1.0f - Mathf.Sin(distanceTraveled * (1.0f / stepLength) / 2)) * stepHeight * 0.4f);
+
+        if ((1.0f - Mathf.Sin(distanceTraveled * (1.0f / stepLength))) <= 0.1f)
+        {
+            if (oneStep)
+            {
+                oneStep = false;
+                // footstepNoise.m_noise = (isSneaking ? sneakFootstep : normalFootstep);
+                // footstepNoise.PlayNoise();
+                Step.Invoke();
+            }
+        }
+        else
+        {
+            oneStep = true;
         }
 
         // Debug
